225add332df220296eecfcfaebe683c9fe3fb650
brew <brew@cwv.io>
2020-06-02 02:29:58 +0800

    add loc

--- astart.sh
+++ astart.sh
@@ -7,6 +7,7 @@ nohup java -Xmx16G -Xmx16G -DRUN_PATH=`pwd` -cp sbin/felix.jar:sbin/protobuf-jav
 	 -Dorg.apache.felix.http.jetty.acceptors=64 \
 	 -Dorg.bc.pzp.networks.loc.id=$HOSTNAME \
 	 -Dorg.bc.pzp.networks.loc.gwuris=$HOSTNAME:5100 \
+	 -Dorg.bc.pzp.networks.vrf.nodelist="tcp://fc0:5100,tcp://fc1:5100,tcp://fc2:5100,tcp://fc3:5100,tcp://fc4:5100" \
 	 -Dlogback.configurationFile=conf/logback-server.xml org.apache.felix.main.Main &
 
 
