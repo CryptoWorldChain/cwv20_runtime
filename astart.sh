48d56ed8f64fa5c41429cbf6f8ec7f729beb81e9
brew <brew@cwv.io>
2020-06-01 12:23:18 +0800

    change log

--- astart.sh
+++ astart.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 nohup java -Xmx8G -DRUN_PATH=`pwd` -cp sbin/felix.jar:sbin/protobuf-java-3.6.1.jar -DofwConf=conf/ofw.properties \
+	 -Dorg.bc.dpos.force.reset.vote.term=1 -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:+UseAdaptiveSizePolicy \  
+	 -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -Duser.timezone=GMT+08 \
+	 -Dorg.brewchain.man.dev=false  -Dorg.bc.manage.node.net=prodnet \
 	 -Dlogback.configurationFile=conf/logback-server.xml org.apache.felix.main.Main &
 
 
